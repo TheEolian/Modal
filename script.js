f4c1ec8cbf03f761c3168d91ac5902b7e8cb6e23
Michael Soldmann <msoldmann@gmail.com>
2017-09-04 16:51:30 -0500

    added close modal functions

--- script.js
+++ script.js
@@ -3,12 +3,30 @@ var modal = document.getElementById('simpleModal');
 // Get open modal button
 var modalBtn = document.getElementById('modalBtn');
 // Get close button
-var closeBtn = document.getElementById('closeBtn');
+var closeBtn = document.getElementsByClassName('closeBtn')[0];
 
-// Listen for click on modal button and run function openModal
+// Listen for open click on modal button and run function openModal
 modalBtn.addEventListener('click', openModal);
 
-// openModal function
+// Listen for close click on modal button and run function closeModal
+closeBtn.addEventListener('click', closeModal);
+
+// Listen for outside click
+window.addEventListener('click', clickOutside);
+
+// Function to open model
 function openModal(){
   modal.style.display = 'block';
+}
+
+// Function to close modal
+function closeModal(){
+  modal.style.display = 'none';
+}
+
+// Function to close modal if click outside modal
+function clickOutside(x){
+  if(x.target == modal){
+    modal.style.display = 'none';
+  }
 }
